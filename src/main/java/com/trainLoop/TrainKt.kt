aa95ac8648c47d887be2383df179b5b3cc519d2c
ruby <cairulin@hotmail.com>
2022-11-26 22:57:22 +0800

    bookAgain

--- src/main/java/com/trainLoop/TrainKt.kt
+++ src/main/java/com/trainLoop/TrainKt.kt
@@ -3,21 +3,21 @@ package com.trainLoop
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
+//    val scanner = Scanner(System.`in`)
     var bookAgain = 0;
 
     while (bookAgain != -1) {
         println("請問您想要購買幾張車票?")
-        val totalTicket = scanner.nextInt();
+        val totalTicket = readLine()!!.toInt()
         println("請問有幾張是來回票?")
-        val returnTicket = scanner.nextInt();
+        val returnTicket = readLine()!!.toInt()
 
         val t = TrainKt(totalTicket, returnTicket)
         t.setting()
         t.totalPrice()
 
         println("還需要繼續訂票嗎?（要，請輸入任意數字；不要，請輸入-1。）")
-        bookAgain = scanner.nextInt()
+        bookAgain = readLine()!!.toInt()
 
         if (bookAgain == -1) {
             println("謝謝您的惠顧，歡迎下次蒞臨!")
